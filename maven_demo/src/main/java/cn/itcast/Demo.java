0ed6184c6338ee171b2dc8525a64352b20d86d42
123 <123@163.com>
2018-11-25 11:46:53 +0800

    genggaiewret

--- maven_demo/src/main/java/cn/itcast/Demo.java
+++ maven_demo/src/main/java/cn/itcast/Demo.java
@@ -5,5 +5,10 @@ public class Demo {
         System.out.println("aaaaaa");
 		System.out.println("bbbb");
         System.out.println("ccccc");
+
+        System.out.println("idea");
+
+		System.out.println("xiaowugui");
+
     }
 }
