cb3c711069ca8c56f8f492f97363f130317896c6
123 <123@163.com>
2018-11-25 11:41:19 +0800

    sadfg

--- maven_demo/src/main/java/cn/itcast/Demo.java
+++ maven_demo/src/main/java/cn/itcast/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("aaaaaa");
 		System.out.println("bbbb");
+        System.out.println("ccccc");
     }
 }
